a1472c5c658df7d9c726dcd5e3d3cd95e9f343cd
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-03-03 22:38:26 -0500

    Add my_each_with_index, my_select, my_all and my_any methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,52 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    count = 0
+    self.my_each do |value|
+      yield(value,count)
+      count += 1
+    end
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    store = []
+    self.my_each do |value|
+      if yield(value)
+        store << value
+      end
+    end
+    store
+  end
+
+  def my_all? 
+    self.my_each do |value|
+      if block_given?
+        if !yield(value)
+          return false
+        end
+      elsif !value
+        return false
+      end  
+    end
+    true
+  end
+
+  def my_any?
+        self.my_each do |value|
+      if block_given?
+        if yield(value)
+          return true
+        end
+      elsif value
+        return true
+      end  
+    end
+    false
+  end
 end
 
 # You will first have to define my_each
